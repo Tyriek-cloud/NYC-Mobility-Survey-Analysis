29823583a75b74aa4b44584da17bd01b58b1a9f6
Tyriek Warren <62261407+Tyriek-cloud@users.noreply.github.com>
2024-06-22 15:18:05 -0400

    Update NYC-Mobility-Survey-Analysis-job.py

--- NYC-Mobility-Survey-Analysis-job.py
+++ NYC-Mobility-Survey-Analysis-job.py
@@ -4,6 +4,7 @@ from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
+from awsglue.dynamicframe import DynamicFrame
 
 args = getResolvedOptions(sys.argv, ['JOB_NAME'])
 sc = SparkContext()
@@ -28,15 +29,23 @@ Trip_node1719078288622 = glueContext.create_dynamic_frame.from_options(format_op
 Household_node1719078285940 = glueContext.create_dynamic_frame.from_options(format_options={"quoteChar": "\"", "withHeader": True, "separator": ",", "optimizePerformance": False}, connection_type="s3", format="csv", connection_options={"paths": ["s3://nyc-mobility-survey-analysis/staging/Citywide_Mobility_Survey_-_Household_2022_20240622.csv"], "recurse": True}, transformation_ctx="Household_node1719078285940")
 
 # Script generated for node Join_Vehicle_Person
-Join_Vehicle_Person_node1719078697666 = Join.apply(frame1=Vehicle_node1719078290090, frame2=Person_node1719078286883, keys1=["hh_id"], keys2=["hh_id"], transformation_ctx="Join_Vehicle_Person_node1719078697666")
+Vehicle_node1719078290090DF = Vehicle_node1719078290090.toDF()
+Person_node1719078286883DF = Person_node1719078286883.toDF()
+Join_Vehicle_Person_node1719078697666 = DynamicFrame.fromDF(Vehicle_node1719078290090DF.join(Person_node1719078286883DF, (Vehicle_node1719078290090DF['hh_id'] == Person_node1719078286883DF['hh_id']), "outer"), glueContext, "Join_Vehicle_Person_node1719078697666")
 
 # Script generated for node Join_Person_Day
-Join_Person_Day_node1719078514550 = Join.apply(frame1=Day_node1719078284552, frame2=Person_node1719078286883, keys1=["person_id"], keys2=["person_id"], transformation_ctx="Join_Person_Day_node1719078514550")
+Day_node1719078284552DF = Day_node1719078284552.toDF()
+Person_node1719078286883DF = Person_node1719078286883.toDF()
+Join_Person_Day_node1719078514550 = DynamicFrame.fromDF(Day_node1719078284552DF.join(Person_node1719078286883DF, (Day_node1719078284552DF['person_id'] == Person_node1719078286883DF['person_id']), "outer"), glueContext, "Join_Person_Day_node1719078514550")
 
 # Script generated for node Join_Trip_Vehicle
-Join_Trip_Vehicle_node1719078854554 = Join.apply(frame1=Trip_node1719078288622, frame2=Vehicle_node1719078290090, keys1=["hh_id"], keys2=["hh_id"], transformation_ctx="Join_Trip_Vehicle_node1719078854554")
+Trip_node1719078288622DF = Trip_node1719078288622.toDF()
+Vehicle_node1719078290090DF = Vehicle_node1719078290090.toDF()
+Join_Trip_Vehicle_node1719078854554 = DynamicFrame.fromDF(Trip_node1719078288622DF.join(Vehicle_node1719078290090DF, (Trip_node1719078288622DF['hh_id'] == Vehicle_node1719078290090DF['hh_id']), "outer"), glueContext, "Join_Trip_Vehicle_node1719078854554")
 
 # Script generated for node Join_Household_Person
-Join_Household_Person_node1719078578757 = Join.apply(frame1=Household_node1719078285940, frame2=Person_node1719078286883, keys1=["hh_id"], keys2=["hh_id"], transformation_ctx="Join_Household_Person_node1719078578757")
+Household_node1719078285940DF = Household_node1719078285940.toDF()
+Person_node1719078286883DF = Person_node1719078286883.toDF()
+Join_Household_Person_node1719078578757 = DynamicFrame.fromDF(Household_node1719078285940DF.join(Person_node1719078286883DF, (Household_node1719078285940DF['hh_id'] == Person_node1719078286883DF['hh_id']), "outer"), glueContext, "Join_Household_Person_node1719078578757")
 
 job.commit()
